b2f296133eacdc4154e632754ef82a98ef7d5340
minhaz11 <minhaz217@gmail.com>
2023-01-19 17:25:11 +0600

    Project Done

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,39 @@
 <template>
 
  <h1>Reaction Timer</h1>
- <button @click="start" class="btn btn-primary mt-2" :disabled="playing">Play</button>
+ <button @click="start" class="btn btn-success mt-2" :disabled="playing">Play</button>
+ <Block v-if="playing" :delay="delay" @end="endGame"/>
 
- <Block v-if="playing" :delay="delay"/>
+ <Results v-if="showResult"  :score="score" />
 
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: {Block},
+  components: {Block,Results},
   data() {
     return {
       time: 0,
       delay: null,
       playing: false,
       score: 0,
-      highScore: 0
+      showResult: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.playing = true;
+      this.showResult = false;
+    },
+    endGame(reactionTime){
+      this.score = reactionTime;
+      this.playing = false;
+      this.showResult = true;
     }
   }
 
---
+++ src/components/Results.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <p class="mt-2">Reaction time : {{ score }} ms</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['score']
+    
+}
+</script>
+<style>
+    
+</style>
